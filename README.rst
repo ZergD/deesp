9b0fc49268b90cf18e3967dcad4daba0da6e7c07
MOZGAWA Marc Ext <mozgawamar@GRNATL0343.bureau.si.interne>
2019-03-01 12:24:00 +0100

    second commit with setup and readme

---
+++ README.rst
@@ -0,0 +1,10 @@
+Deesp
+------------
+
+To use (with caution), simply do::
+
+   >>> import deesp
+   >>> print(deesp.blague)
+
+Useful cmds::
+for installing locally: python3 setup.py develop
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='deesp',
       version='0.1',
       description='The best Dispatcher in the universe',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/ZergD/deesp',
       author='MarcM',
       author_email='marc.mozgawa@gmail.com',
       license='LGP-LV3',
       packages=['deesp'],
+      install_requires=[
+          'markdown',
+      ],
       zip_safe=False)
