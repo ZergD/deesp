90fb4cfb507977cacc34bdd2b9b99986b077956f
MOZGAWA Marc Ext <mozgawamar@GRNATL0343.bureau.si.interne>
2019-03-01 11:48:54 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deesp',
+      version='0.1',
+      description='The best Dispatcher in the universe',
+      url='http://github.com/storborg/funniest',
+      author='MarcM',
+      author_email='marc.mozgawa@gmail.com',
+      license='LGP-LV3',
+      packages=['deesp'],
+      zip_safe=False)
