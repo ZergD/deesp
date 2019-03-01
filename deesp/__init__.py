86303a9dd2fa93097135101a29121084bc4c9f49
MOZGAWA Marc Ext <mozgawamar@GRNATL0343.bureau.si.interne>
2019-03-01 16:29:56 +0100

    refactoring

--- deesp/__init__.py
+++
@@ -1,6 +0,0 @@
-def autorun():
-    return("Hello ! Metarch deesp initiated")
-
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
--- deesp/text.py
+++ deesp/text.py
@@ -1,7 +1,16 @@
 from markdown import markdown
 
+
 def blague():
     return markdown(u'Wenn ist das Nunst\u00fcck git und Slotermeyer?'
                     u'Ja! ... **Beiherhund** das Oder die Flipperwaldt '
                     u'gersput.')
 
+
+def autorun():
+    return "Hello ! Metarch deesp initiated"
+
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
